a4421e079e97bd6b425463b653f22a3ede42a0e8
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2021-01-05 15:24:03 +0500

    Connect to ws://echo.websocket.org (async), free functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <iomanip>
+#include <string_view>
 
 #include <boost/system/error_code.hpp>
 
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/connect.hpp>
+#include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
-
 
 namespace asio = boost::asio;
 namespace beast = boost::beast;
@@ -16,34 +18,126 @@ constexpr auto kHost{"echo.websocket.org"};
 constexpr auto kPort{"80"};
 
 
-// void LogError(const boost::system::error_code ec)
-// {
-//     std::cerr << "Error: " << ec.message() << std::endl;
-// }
+void LogError(const std::string_view where, const beast::error_code& ec)
+{
+    std::cerr << '[' << std::setw(20) << where << "]Error: " << ec.message() << std::endl;
+}
+
+
+void OnReceive(
+    // lambda
+    beast::flat_buffer &receive_buffer,
+    // args
+    const beast::error_code &ec
+)
+{
+    if (ec) {
+        LogError("OnReceive", ec);
+        return;
+    }
+    std::cout << "ECHO: " << beast::make_printable(receive_buffer.data()) << std::endl;
+}
+
+void OnSend(
+    // lambda
+    beast::websocket::stream<beast::tcp_stream> &tcp_ws,
+    beast::flat_buffer &receive_buffer,
+    // args
+    const beast::error_code &ec
+)
+{
+    if (ec) {
+        LogError("OnSend", ec);
+        return;
+    }
+    tcp_ws.async_read(receive_buffer,
+        [&receive_buffer](auto ec, auto bytes_read){
+            OnReceive(receive_buffer, ec);
+        }
+    );
+}
+
+void OnHandshake(
+    // lambda
+    beast::websocket::stream<beast::tcp_stream> &tcp_ws,
+    const asio::const_buffer &send_buffer,
+    beast::flat_buffer &receive_buffer,
+    // args
+    const beast::error_code &ec
+)
+{
+    if (ec) {
+        LogError("OnHandshake", ec);
+        return;
+    }
+    tcp_ws.async_write(send_buffer,
+        [&tcp_ws, &receive_buffer](auto ec, auto bytes_written){
+            OnSend(tcp_ws, receive_buffer, ec);
+        }
+    );
+}
+
+void OnConnect(
+    // lambda
+    beast::websocket::stream<beast::tcp_stream> &tcp_ws,
+    const std::string_view url,
+    const asio::const_buffer &send_buffer,
+    beast::flat_buffer &receive_buffer,
+    // args
+    const beast::error_code &ec
+)
+{
+    if (ec) {
+        LogError("OnConnect", ec);
+        return;
+    }
+    tcp_ws.async_handshake(url, "/",
+        [&tcp_ws, &send_buffer, &receive_buffer](auto ec){
+            OnHandshake(tcp_ws, send_buffer, receive_buffer, ec);
+        }
+    );
+}
+
+void OnResolve(
+    // lambda
+    beast::websocket::stream<beast::tcp_stream> &tcp_ws,
+    const std::string_view url,
+    const asio::const_buffer &send_buffer,
+    beast::flat_buffer &receive_buffer,
+    // args
+    const beast::error_code& ec,
+    asio::ip::tcp::resolver::results_type endpoints
+)
+{
+    if (ec) {
+        LogError("OnResolve", ec);
+        return;
+    }
+    tcp_ws.next_layer().async_connect(endpoints,
+        [&tcp_ws, url, &send_buffer, &receive_buffer](auto ec, auto _){
+            OnConnect(tcp_ws, url, send_buffer, receive_buffer, ec);
+        }
+    );
+}
 
 
 int main()
 {
     try {
         asio::io_context asio_context{};
-        beast::websocket::stream<asio::ip::tcp::socket> tcp_websocket{asio_context};
+        beast::websocket::stream<beast::tcp_stream> tcp_wss{asio_context};
 
-        const auto connected_endpoint = asio::connect(
-            tcp_websocket.next_layer(),
-            asio::ip::tcp::resolver{asio_context}.resolve(kHost, kPort)
-        );
-        tcp_websocket.handshake(kHost, "/");
-
-        tcp_websocket.text(true); // NOTE: Why???
         asio::const_buffer send_buffer{"Echo message", 13};
-        tcp_websocket.write(send_buffer);
+        beast::flat_buffer recieve_buffer{};
 
-        beast::flat_buffer response_buffer{};
-        tcp_websocket.read(response_buffer);
+        asio::ip::tcp::resolver resolver{asio_context};
+        resolver.async_resolve(kHost, kPort,
+            [&tcp_wss, &send_buffer, &recieve_buffer](auto ec, auto endpoints){
+                OnResolve(tcp_wss, kHost, send_buffer, recieve_buffer, ec, endpoints);
+            }
+        );
 
-        tcp_websocket.close(beast::websocket::close_code::normal);
-
-        std::cout << beast::make_printable(response_buffer.data()) << std::endl;
+        asio_context.run();
     }
     catch (std::exception& e)
     {
